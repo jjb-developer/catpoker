c85fa4a693ce42de3d9359001fe3bb3c5bed1028
jose pereira <andrito1985@gmail.com>
2024-04-19 20:41:14 -0400

    feat(activar jugadores) activar jugadores al hacer click en un asiento vacio y tiempo para todas las rondas de apuestas.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 import { $ } from './src/utils/funciones.js'
 import { TAPE1, TAPE2, FIGURAS, CARTAS } from './src/utils/constantes.js'
 
+let participantes = 0
 
 const p1 = $('#p1')
 const p2 = $('#p2')
@@ -9,6 +10,106 @@ const p3 = $('#p3')
 const p4 = $('#p4')
 const p5 = $('#p5')
 const p6 = $('#p6')
+
+let start = false
+
+p1.addEventListener('click', ()=>{
+
+})
+
+p2.addEventListener('click', ()=>{
+  if(board[1] === 'vacio' && board[7] === 'vacio'){
+    board[1] = 'esperando'
+    board[7] = 'esperando'
+    participantes += 1
+    console.info(board)
+  }
+  if(participantes >= 2 && start === false){
+    start = true
+    poker()
+  }
+})
+
+p3.addEventListener('click', ()=>{
+  if(board[2] === 'vacio' && board[8] === 'vacio'){
+    board[2] = 'esperando'
+    board[8] = 'esperando'
+    participantes += 1
+    console.info(board)
+  }
+  if(participantes >= 2 && start === false){
+    start = true
+    poker()
+  }
+})
+
+p4.addEventListener('click', ()=>{
+  if(board[3] === 'vacio' && board[9] === 'vacio'){
+    board[3] = 'esperando'
+    board[9] = 'esperando'
+    participantes += 1
+    console.info(board)
+  }
+  if(participantes >= 2 && start === false){
+    start = true
+    poker()
+  }
+})
+
+p5.addEventListener('click', ()=>{
+  if(board[4] === 'vacio' && board[10] === 'vacio'){
+    board[4] = 'esperando'
+    board[10] = 'esperando'
+    participantes += 1
+    console.info(board)
+  }
+  if(participantes >= 2 && start === false){
+    start = true
+    poker()
+  }
+})
+
+p6.addEventListener('click', ()=>{
+  start = true
+  if(board[5] === 'vacio' && board[11] === 'vacio'){
+    board[5] = 'esperando'
+    board[11] = 'esperando'
+    participantes += 1
+    console.info(board)
+  }
+  if(participantes >= 2 && start === false){
+    start = true
+    poker()
+  }
+})
+
+const seat_p1 = $('#seat_p1')
+const salir_p1 = $('#salir_p1')
+const name_p1 = $('#name_p1')
+const coins_p1 = $('#coins_p1')
+seat_p1.addEventListener('click',()=>{
+  name_p1.innerText = 'andrew'
+  coins_p1.innerText = 5000
+  seat_p1.classList.add('hidden')
+  salir_p1.classList.remove('hidden')
+  if(board[0] === 'vacio' && board[6] === 'vacio'){
+    board[0] = 'esperando'
+    board[6] = 'esperando'
+    participantes += 1
+    console.info(board)
+  }
+  if(participantes >= 2 && start === false){
+    start = true
+    poker()
+  }
+})
+
+salir_p1.addEventListener('click',()=>{
+  name_p1.innerText = ''
+  coins_p1.innerText = ''
+  seat_p1.classList.remove('hidden')
+  salir_p1.classList.add('hidden')
+})
 
 const j1c1 = $('#j1c1')
 const j2c1 = $('#j2c1')
@@ -32,11 +133,12 @@ const river = $('#river')
 let indices = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51]
 
 
-let board = [0,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true]
+let board = [
+'vacio','vacio','vacio','vacio','vacio','vacio',
+'vacio','vacio','vacio','vacio','vacio','vacio',
+'','','','','']
 
-
-let jugadores = [0,1,2,3,4,5]
-let turno_de = null
+let cronometro = $('#cronometro')
 
 /*
 const pos_j1 = [0,6]
@@ -47,12 +149,11 @@ const pos_j5 = [4,10]
 const pos_j6 = [5,11]
 */
 
-let time_j1 = 60
-let time_j2 = 60
-let time_j3 = 60
-let time_j4 = 60
-let time_j5 = 60
-let time_j6 = 60
+const SEGUNDOS_FOR_BET = 3
+const INTERVALO_SHOW_CARDS = 0.175
+
+let intervalo_para_apostar = SEGUNDOS_FOR_BET
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -68,7 +169,7 @@ function setCard(i){
 
 function repartir(){
   for(let i=0; i<17; i++){
-    if(board[i] !== -1) setCard(i)
+    if(board[i] !== 'vacio' && board[i] !== 'ausente' && board[i] !== 'esperando') setCard(i)
   }
   //console.info(board)
 }
@@ -96,205 +197,153 @@ function clearBoard(){
 }
 
 
-async function showCards(){
-  return new Promise(resolve=>{
-    if(board[0] !== -1){
+function getBoardElement(i,player){
+  return new Promise((resolve,reject)=>{
+    if(typeof(board[i]) === 'number'){
       setTimeout(()=>{
-        j1c1.innerHTML = FIGURAS[board[0]]
-      },1000)
+        player.innerHTML = FIGURAS[board[i]]
+        resolve('listo, elemento renderizado.')
+      },INTERVALO_SHOW_CARDS*1000)
+    } else {
+      resolve('nada que renderizar.')
     }
-
-    if(board[1] !== -1){
-      setTimeout(()=>{
-        j2c1.innerHTML = FIGURAS[board[1]]
-      },1500)
-    }
-
-    if(board[2] !== -1){
-      setTimeout(()=>{
-        j3c1.innerHTML = FIGURAS[board[2]]
-      },2000)
-    }
-    if(board[3] !== -1){
-      setTimeout(()=>{
-        j4c1.innerHTML = FIGURAS[board[3]]
-      },2500)
-    }
-    if(board[4] !== -1){
-      setTimeout(()=>{
-        j5c1.innerHTML = FIGURAS[board[4]]
-      },3000)
-    }
-    if(board[5] !== -1){
-      setTimeout(()=>{
-        j6c1.innerHTML = FIGURAS[board[5]]
-      },3500)
-    }
-    if(board[6] !== -1){
-      setTimeout(()=>{
-        j1c2.innerHTML = FIGURAS[board[6]]
-      },4000)
-    }
-    if(board[7] !== -1){
-      setTimeout(()=>{
-        j2c2.innerHTML = FIGURAS[board[7]]
-      },4500)
-    }
-    if(board[8] !== -1){
-      setTimeout(()=>{
-        j3c2.innerHTML = FIGURAS[board[8]]
-      },5000)
-    }
-    if(board[9] !== -1){
-      setTimeout(()=>{
-        j4c2.innerHTML = FIGURAS[board[9]]
-      },5500)
-    }
-    if(board[10] !== -1){
-      setTimeout(()=>{
-        j5c2.innerHTML = FIGURAS[board[10]]
-      },6000)
-    }
-
-    if(board[11] !== -1){
-      setTimeout(()=>{
-        j6c2.innerHTML = FIGURAS[board[11]]
-      },6500)
-    }
-
-    // RONDA DE APUESTAS PRE-FLOP
-    if(board[0] !== -1){
-      setTimeout(()=>{
-        p1.classList.add('border-lime-500')
-        turno_de = 0
-        console.info(turno_de)
-      },(6500+(800*1)))
-    }
-
-    if(board[1] !== -1){
-      setTimeout(()=>{
-        p1.classList.remove('border-lime-500')
-        p2.classList.add('border-lime-500')
-        turno_de = 1
-        console.info(turno_de)
-      },(6500+(800*2)))
-    }
-
-    if(board[2] !== -1){
-      setTimeout(()=>{
-        p2.classList.remove('border-lime-500')
-        p3.classList.add('border-lime-500')
-        turno_de = 2
-        console.info(turno_de)
-      },(6500+(800*3)))
-    }
-
-    if(board[3] !== -1){
-      setTimeout(()=>{
-        p3.classList.remove('border-lime-500')
-        p4.classList.add('border-lime-500')
-        turno_de = 3
-        console.info(turno_de)
-      },(6500+(800*4)))
-    }
-
-    if(board[4] !== -1){
-      setTimeout(()=>{
-        p4.classList.remove('border-lime-500')
-        p5.classList.add('border-lime-500')
-        turno_de = 4
-        console.info(turno_de)
-      },(6500+(800*5)))
-    }
-
-    if(board[5] !== -1){
-      setTimeout(()=>{
-        p5.classList.remove('border-lime-500')
-        p6.classList.add('border-lime-500')
-        turno_de = 5
-        console.info(turno_de)
-      },(6500+(800*6)))
-    }
-
-    // PRE-FLOP
-
-    // FLOP
-    if(board[12] !== -1){
-      setTimeout(()=>{
-        p6.classList.remove('border-lime-500')
-        flop1.innerHTML = `<span class='text-[2.5rem]'>${FIGURAS[board[12]]}</span>`
-      },12000)
-    }
-
-    if(board[13] !== -1){
-      setTimeout(()=>{
-        flop2.innerHTML = `<span class='text-[2.5rem]'>${FIGURAS[board[13]]}</span>`
-      },12500)
-    }
-
-    if(board[14] !== -1){
-      setTimeout(()=>{
-        flop3.innerHTML = `<span class='text-[2.5rem]'>${FIGURAS[board[14]]}</span>`
-      },13000)
-    }
-
-    // RONDA DE APUESTAS FLOP
-    setTimeout(()=>{
-
-    },18000)
-
-    // TURN
-    if(board[15] !== -1){
-      setTimeout(()=>{
-        turn.innerHTML = `<span class='text-[2.5rem]'>${FIGURAS[board[15]]}</span>`
-      },18500)
-    }
-
-    // RONDA DE APUESTAS TURN
-    setTimeout(()=>{
-
-    },23500)
-
-    // RIVER
-    if(board[16] !== -1){
-      setTimeout(()=>{
-        river.innerHTML = `<span class='text-[2.5rem]'>${FIGURAS[board[16]]}</span>`
-      },24000)
-    }
-
-    // RONDA DE APUESTAS RIVER
-    setTimeout(()=>{
-
-    },29000)
-
-    setTimeout(()=>{
-      resolve("Ha finalizado la mano!.")
-    },31000)
-
   })
 }
+
+function apuesta(player){
+  return new Promise((resolve,reject)=>{
+    const intervalo = setInterval(()=>{
+      intervalo_para_apostar -= 1
+      cronometro.classList.remove('hidden')
+      cronometro.innerHTML = intervalo_para_apostar
+    }, 1000);
+    setTimeout(()=>{
+      clearInterval(intervalo)
+      intervalo_para_apostar = SEGUNDOS_FOR_BET
+      cronometro.classList.add('hidden')
+      resolve('jugador ya decidio.')
+    },SEGUNDOS_FOR_BET*1000)
+  })
+}
+
+function baseApuesta(player){
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve('jugador ya decidio.')
+    },2500)
+  })
+}
+
+async function turno(player,c1){
+  if(typeof(board[c1]) === 'number'){
+    player.classList.add('border-lime-500')
+    await apuesta(player)
+    player.classList.remove('border-lime-500')
+    //console.info(`Jugador ${player.id} ya aposto.`)
+  }
+}
+
+async function mostrarCartas(){
+  await getBoardElement(0,j1c1)
+  await getBoardElement(1,j2c1)
+  await getBoardElement(2,j3c1)
+  await getBoardElement(3,j4c1)
+  await getBoardElement(4,j5c1)
+  await getBoardElement(5,j6c1)
+
+  await getBoardElement(6,j1c2)
+  await getBoardElement(7,j2c2)
+  await getBoardElement(8,j3c2)
+  await getBoardElement(9,j4c2)
+  await getBoardElement(10,j5c2)
+  await getBoardElement(11,j6c2)
+
+
+  //console.info('Rondas de apuestas Pre-Flop.')
+  await turno(p1,0)
+  await turno(p2,1)
+  await turno(p3,2)
+  await turno(p4,3)
+  await turno(p5,4)
+  await turno(p6,5)
+
+
+  await getBoardElement(12,flop1)
+  await getBoardElement(13,flop2)
+  await getBoardElement(14,flop3)
+  //console.info('Rondas de apuestas Flop.')
+  await turno(p1,0)
+  await turno(p2,1)
+  await turno(p3,2)
+  await turno(p4,3)
+  await turno(p5,4)
+  await turno(p6,5)
+
+
+  await getBoardElement(15,turn)
+  //console.info('Rondas de apuestas Turn.')
+  await turno(p1,0)
+  await turno(p2,1)
+  await turno(p3,2)
+  await turno(p4,3)
+  await turno(p5,4)
+  await turno(p6,5)
+
+
+  await getBoardElement(16,river)
+  //console.info('Rondas de apuestas River.')
+  await turno(p1,0)
+  await turno(p2,1)
+  await turno(p3,2)
+  await turno(p4,3)
+  await turno(p5,4)
+  await turno(p6,5)
+}
+
 
 let play = true
 
 const salir = $('#salir')
 salir.addEventListener('click',()=>{
-  play = false
-  //console.log(play)
+  play = !play
+  participantes -= 1
+  start = false
 })
 
-for(let i=0;i<2;i++){
+
+function run(){
+  if(board.slice(0,6).filter(e=>typeof(e) === 'number').length>1){
+    return true
+  }
+  return false
+}
+
+function updateBoard(){
+  board = board.map(e=>{
+    if(e==='esperando'){
+      return 0
+    } else {
+      return e
+    }
+  })
+}
+
+
+async function poker(){
+  while(participantes >= 2){
+    updateBoard()
     repartir()
-    await showCards()
+    await mostrarCartas()
     clearBoard()
+  }
 }
 
 /*
 
-while(play){
-    repartir()
-    await showCards()
-    clearBoard()
+for(let i=0; i<5; i++){
+  repartir()
+  await mostrarCartas()
+  clearBoard()
 }
-
 
 */
--- src/utils/funciones.js
+++ src/utils/funciones.js
@@ -37,3 +37,182 @@ export function repartirRiver(indices,board){
     river.innerHTML = board[4] ? `<span class='text-[2.5rem]'>${FIGURAS[board[4]]}</span>` : ''
   },3000)
 }
+
+
+async function showCards(){
+  return new Promise(resolve=>{
+    if(board[0] !== -1){
+      setTimeout(()=>{
+        j1c1.innerHTML = FIGURAS[board[0]]
+      },1000)
+    }
+
+    if(board[1] !== -1){
+      setTimeout(()=>{
+        j2c1.innerHTML = FIGURAS[board[1]]
+      },1500)
+    }
+
+    if(board[2] !== -1){
+      setTimeout(()=>{
+        j3c1.innerHTML = FIGURAS[board[2]]
+      },2000)
+    }
+    if(board[3] !== -1){
+      setTimeout(()=>{
+        j4c1.innerHTML = FIGURAS[board[3]]
+      },2500)
+    }
+    if(board[4] !== -1){
+      setTimeout(()=>{
+        j5c1.innerHTML = FIGURAS[board[4]]
+      },3000)
+    }
+    if(board[5] !== -1){
+      setTimeout(()=>{
+        j6c1.innerHTML = FIGURAS[board[5]]
+      },3500)
+    }
+    if(board[6] !== -1){
+      setTimeout(()=>{
+        j1c2.innerHTML = FIGURAS[board[6]]
+      },4000)
+    }
+    if(board[7] !== -1){
+      setTimeout(()=>{
+        j2c2.innerHTML = FIGURAS[board[7]]
+      },4500)
+    }
+    if(board[8] !== -1){
+      setTimeout(()=>{
+        j3c2.innerHTML = FIGURAS[board[8]]
+      },5000)
+    }
+    if(board[9] !== -1){
+      setTimeout(()=>{
+        j4c2.innerHTML = FIGURAS[board[9]]
+      },5500)
+    }
+    if(board[10] !== -1){
+      setTimeout(()=>{
+        j5c2.innerHTML = FIGURAS[board[10]]
+      },6000)
+    }
+
+    if(board[11] !== -1){
+      setTimeout(()=>{
+        j6c2.innerHTML = FIGURAS[board[11]]
+      },6500)
+    }
+
+    // RONDA DE APUESTAS PRE-FLOP
+    if(board[0] !== -1){
+      setTimeout(()=>{
+        p1.classList.add('border-lime-500')
+        turno_de = 0
+        console.info(turno_de)
+      },(6500+(800*1)))
+    }
+
+    if(board[1] !== -1){
+      setTimeout(()=>{
+        p1.classList.remove('border-lime-500')
+        p2.classList.add('border-lime-500')
+        turno_de = 1
+        console.info(turno_de)
+      },(6500+(800*2)))
+    }
+
+    if(board[2] !== -1){
+      setTimeout(()=>{
+        p2.classList.remove('border-lime-500')
+        p3.classList.add('border-lime-500')
+        turno_de = 2
+        console.info(turno_de)
+      },(6500+(800*3)))
+    }
+
+    if(board[3] !== -1){
+      setTimeout(()=>{
+        p3.classList.remove('border-lime-500')
+        p4.classList.add('border-lime-500')
+        turno_de = 3
+        console.info(turno_de)
+      },(6500+(800*4)))
+    }
+
+    if(board[4] !== -1){
+      setTimeout(()=>{
+        p4.classList.remove('border-lime-500')
+        p5.classList.add('border-lime-500')
+        turno_de = 4
+        console.info(turno_de)
+      },(6500+(800*5)))
+    }
+
+    if(board[5] !== -1){
+      setTimeout(()=>{
+        p5.classList.remove('border-lime-500')
+        p6.classList.add('border-lime-500')
+        turno_de = 5
+        console.info(turno_de)
+      },(6500+(800*6)))
+    }
+
+    // PRE-FLOP
+
+    // FLOP
+    if(board[12] !== -1){
+      setTimeout(()=>{
+        p6.classList.remove('border-lime-500')
+        flop1.innerHTML = `<span class='text-[2.5rem]'>${FIGURAS[board[12]]}</span>`
+      },12000)
+    }
+
+    if(board[13] !== -1){
+      setTimeout(()=>{
+        flop2.innerHTML = `<span class='text-[2.5rem]'>${FIGURAS[board[13]]}</span>`
+      },12500)
+    }
+
+    if(board[14] !== -1){
+      setTimeout(()=>{
+        flop3.innerHTML = `<span class='text-[2.5rem]'>${FIGURAS[board[14]]}</span>`
+      },13000)
+    }
+
+    // RONDA DE APUESTAS FLOP
+    setTimeout(()=>{
+
+    },18000)
+
+    // TURN
+    if(board[15] !== -1){
+      setTimeout(()=>{
+        turn.innerHTML = `<span class='text-[2.5rem]'>${FIGURAS[board[15]]}</span>`
+      },18500)
+    }
+
+    // RONDA DE APUESTAS TURN
+    setTimeout(()=>{
+
+    },23500)
+
+    // RIVER
+    if(board[16] !== -1){
+      setTimeout(()=>{
+        river.innerHTML = `<span class='text-[2.5rem]'>${FIGURAS[board[16]]}</span>`
+      },24000)
+    }
+
+    // RONDA DE APUESTAS RIVER
+    setTimeout(()=>{
+
+    },29000)
+
+    setTimeout(()=>{
+      resolve("Ha finalizado la mano!.")
+    },31000)
+
+  })
+}
